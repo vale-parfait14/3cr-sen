6c033851ded113c88f0abc998b17dd3c101ed721
Parfait OVALE ASSOUME <163141468+vale-parfait14@users.noreply.github.com>
2024-12-04 10:26:19 +0000

    Update Fichier.js

--- client/src/components/Fichier.js
+++ client/src/components/Fichier.js
@@ -331,7 +331,7 @@ const PatientSolvable = ({ patients }) => {
             <Form.Group className="mb-3">
   <Form.Label>Document(Dropbox)</Form.Label>
   <DropboxChooser 
-    appKey="dh9gtv3tojipp6o"
+    appKey="23rlajqskcae2gk"
     success={handleDropboxSuccess}
     cancel={() => toast.info('Sélection annulée')}
     multiselect={true}
