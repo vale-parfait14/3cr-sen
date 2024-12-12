75cade9b6f210537b167f6e7b6291123e3e8c1f4
Parfait OVALE ASSOUME <163141468+vale-parfait14@users.noreply.github.com>
2024-12-12 15:40:09 +0000

    Update Authentifications.js

--- client/src/components/Authentifications.js
+++ client/src/components/Authentifications.js
@@ -16,7 +16,7 @@ const Authentifications = () => {
   useEffect(() => {
     const fetchUsers = async () => {
       try {
-        const response = await axios.get('http://localhost:5002/users'); // API pour récupérer les utilisateurs
+        const response = await axios.get('https://threecr-sen.onrender.com/users'); // API pour récupérer les utilisateurs
         setUsers(response.data);
       } catch (error) {
         toast.error('Erreur lors de la récupération des utilisateurs');
@@ -110,7 +110,7 @@ const handleLogin = async (e) => {
         accessLevel: 'Limité', // Par défaut, accès limité
         service: loginData.service, // Par défaut, service Google
       };
-      await axios.post('http://localhost:5002/users', newUser); // API POST pour ajouter un utilisateur
+      await axios.post('https://threecr-sen.onrender.com/users', newUser); // API POST pour ajouter un utilisateur
       toast.success('Utilisateur créé avec succès');
       setIsLoginMode(true); // Passer en mode connexion après l'inscription
       setLoginData({ name: '', password: '' });
