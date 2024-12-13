658863080ae55c3bc230ee37df7af53cc31c4efe
Parfait OVALE ASSOUME <163141468+vale-parfait14@users.noreply.github.com>
2024-12-13 15:43:46 +0000

    Update Authentifications.js

--- client/src/components/Authentifications.js
+++ client/src/components/Authentifications.js
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import 'react-toastify/dist/ReactToastify.css';
 
+
 const ComposantConnexion = () => {
   const navigate = useNavigate();
   const [donneesConnexion, setDonneesConnexion] = useState({ nom: '', motDePasse: '' });
