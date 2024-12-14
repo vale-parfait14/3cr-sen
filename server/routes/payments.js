7c96e41d1670749b46331fe5039e9d83e96227cb
Parfait OVALE ASSOUME <163141468+vale-parfait14@users.noreply.github.com>
2024-12-14 20:22:20 +0000

    Update payments.js

--- server/routes/payments.js
+++ server/routes/payments.js
@@ -5,38 +5,21 @@ const Payment = require('../models/Payment');
 // Get all payments
 router.get('/', async (req, res) => {
   try {
-    const payments = await Payment.find().populate('patientId'); // Populer les informations du patient
+    const payments = await Payment.find();
     res.json(payments);
   } catch (err) {
-    res.status(500).json({ message: "Erreur lors de la récupération des paiements", error: err.message });
+    res.status(500).json({ message: err.message });
   }
 });
 
 // Add new payment
 router.post('/', async (req, res) => {
-  const { patientId, nom, diagnostic, age, numeroDeTelephone, ordre, datePaiement, statut } = req.body;
-
-  // Validation des champs requis
-  if (!patientId || !nom || !diagnostic || !datePaiement || !ordre) {
-    return res.status(400).json({ message: "Tous les champs requis doivent être remplis." });
-  }
-
-  const payment = new Payment({
-    patientId,
-    nom,
-    diagnostic,
-    age,
-    numeroDeTelephone,
-    ordre,
-    datePaiement,
-    statut: statut || 'Payé' // Le statut par défaut est 'Payé'
-  });
-
+  const payment = new Payment(req.body);
   try {
     const newPayment = await payment.save();
     res.status(201).json(newPayment);
   } catch (err) {
-    res.status(400).json({ message: "Erreur lors de l'enregistrement du paiement", error: err.message });
+    res.status(400).json({ message: err.message });
   }
 });
 
@@ -48,30 +31,21 @@ router.put('/:id', async (req, res) => {
       req.body,
       { new: true }
     );
-
-    if (!updatedPayment) {
-      return res.status(404).json({ message: "Paiement non trouvé" });
-    }
-
     res.json(updatedPayment);
   } catch (err) {
-    res.status(400).json({ message: "Erreur lors de la mise à jour du paiement", error: err.message });
+    res.status(400).json({ message: err.message });
   }
 });
 
 // Delete payment
 router.delete('/:id', async (req, res) => {
   try {
-    const payment = await Payment.findByIdAndDelete(req.params.id);
-
-    if (!payment) {
-      return res.status(404).json({ message: "Paiement non trouvé" });
-    }
-
-    res.json({ message: 'Paiement supprimé avec succès' });
+    await Payment.findByIdAndDelete(req.params.id);
+    res.json({ message: 'Payment deleted successfully' });
   } catch (err) {
-    res.status(500).json({ message: "Erreur lors de la suppression du paiement", error: err.message });
+    res.status(500).json({ message: err.message });
   }
 });
+
 
 module.exports = router;
