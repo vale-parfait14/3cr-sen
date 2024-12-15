ffa3b7013b209f399d07b3f33c791360f584dd48
Parfait OVALE ASSOUME <163141468+vale-parfait14@users.noreply.github.com>
2024-12-15 23:18:31 +0000

    Update Observation.js

--- client/src/components/Observation.js
+++ client/src/components/Observation.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react';
 import DropboxChooser from 'react-dropbox-chooser';
 import { useNavigate } from "react-router-dom";
 import { saveAs } from 'file-saver';
