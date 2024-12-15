ac6d0230be7468a6e0c383a30b1f045bf5cd2224
Parfait OVALE ASSOUME <163141468+vale-parfait14@users.noreply.github.com>
2024-12-15 21:36:27 +0000

    Update Programme.js

--- client/src/components/Programme.js
+++ client/src/components/Programme.js
@@ -209,6 +209,7 @@ const FileManager = () => {
                           : "block",
                           
                     }}
+
                     >
                       Supprimer
                     </button>
