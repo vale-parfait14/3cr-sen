96b639a1cced9f7ab869c3f0c7306885a4be972c
Parfait OVALE ASSOUME <163141468+vale-parfait14@users.noreply.github.com>
2024-12-18 12:27:59 +0000

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -33,7 +33,7 @@ import PasswordPrompt from './components/PasswordPrompt';
 import UserConnection from './components/UserConnection';
 import Programme from './components/Programme';
 import Observation from './components/Observation';
-import filesCro from './components/filesCro';
+import Cro from './components/Cro';
 import filePatient from './components/fichiersPatients';
 import Message from './components/Message';
 import Iconographie from "./components/Iconographie";
@@ -128,7 +128,7 @@ const App = () => {
             <Route path="/userconnection" element={<ProtectedRoute element={<UserConnection token={token} />} token={token} />} />
             <Route path="/programme" element={<ProtectedRoute element={<Programme token={token} />} token={token} />} />
             <Route path="/Observation" element={< Observation element={<Observation token={token} />} token={token} />} />
-             <Route path="/filesCro" element={< filesCro element={<filesCro token={token} />} token={token} />} />
+             <Route path="/fichiers-cro" element={<Cro element={<Cro token={token} />} token={token} />} />
             <Route path="/filepatient" element={<ProtectedRoute element={<filePatient token={token} />} token={token} />} />
 
           </Routes>
