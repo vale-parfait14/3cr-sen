9eeb73b8ede7c1c229ced131eecfec60033e94df
Parfait OVALE ASSOUME <163141468+vale-parfait14@users.noreply.github.com>
2024-12-18 18:02:04 +0000

    Update Cro.js

--- client/src/components/Cro.js
+++ client/src/components/Cro.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './Patients.css';
 
 const Patients = () => {
   const [validatedPatients, setValidatedPatients] = useState([]);
