3c509d27f2330198156ebe9723bf9977e2b7ed51
Parfait OVALE ASSOUME <163141468+vale-parfait14@users.noreply.github.com>
2024-12-19 15:58:31 +0000

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'; // Ajout de Navigate pour redirection
-
 import Auth from './components/Auth'; 
 import Patients from './components/Patients'; 
 import Doctors from './components/Doctors'; 
